c3e985ff6b9c7a91471aa33b94ebad359d158c8f
oluis <luisss.osorio@gmail.com>
2017-11-21 23:47:50 -0200

    Alterado todo list para usar redux e middlewares

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 import IconButton from '../template/iconButton'
-export default props => {
+
+import {remove, markAsDone, markAsPending} from './todoActions'
+
+const TodoList = props => {
   const renderRows = () =>{
     const list = props.list || []
     return list.map(todo => (
@@ -8,11 +13,11 @@ export default props => {
         <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
         <td>
             <IconButton style='success' icon='check' hide={todo.done}
-              onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+              onClick={() => props.markAsDone(todo)}></IconButton>
             <IconButton style='warning' icon='undo' hide={!todo.done}
-              onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+              onClick={() => props.markAsPending(todo)}></IconButton>
             <IconButton style='danger' icon='trash-o' hide={!todo.done}
-              onClick={() => props.handleRemove(todo)}></IconButton>
+              onClick={() => props.remove(todo)}></IconButton>
         </td>
       </tr>
     ))
@@ -31,3 +36,8 @@ export default props => {
     </table>
   )
 }
+
+const mapStateToProps = state => ({list: state.todo.list})
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({remove, markAsDone, markAsPending}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
